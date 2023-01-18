7d3dd1943cad2972f5a38cc5c0096285df8efaf3
Ved Prakash <vedprakash182001@gmail.com>
2023-01-18 11:51:11 +0530

    d

--- src/models/database.js
+++ src/models/database.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose")
 const db = "mongodb://localhost:27017/project5"
-const db1 = process.env.DB
+const db1 = "mongodb+srv://intern:eWIHrTFvXjXiprrY@cluster0.qep6rnh.mongodb.net/interndb?retryWrites=true&w=majority"
 mongoose.set("strictQuery", false);
 mongoose.connect(db1,{
     useNewUrlParser :true,
